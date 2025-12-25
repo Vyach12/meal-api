1f189640675032abf53d2fbe68c68fec961ca96f
Vyach12 <slava.gusarov.73@mail.ru>
2025-12-25 17:41:05 +0300

    added migration scripts

---
+++ migrations/20251225082820_create_categories_table.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_categories_name ON categories(name);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP INDEX IF EXISTS idx_categories_name;
+DROP TABLE IF EXISTS categories;
+-- +goose StatementEnd
